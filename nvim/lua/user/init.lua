d62845206aba4441edb0900f92077fc46a20a3a2
Hung Hoang <hoanghun5@gmail.com>
2024-01-30 11:43:00 +0100

    NVIM keymaps

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,2 +1,4 @@
 require("user.options")
 require("user.lazy")
+require("user.keymaps")
+require("user.keymap_utils")
---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,6 @@
+local nnoremap = require("user.keymap_utils").nnoremap
+local vnoremap = require("user.keymap_utils").vnoremap
+local inoremap = require("user.keymap_utils").inoremap
+local tnoremap = require("user.keymap_utils").tnoremap
+local xnoremap = require("user.keymap_utils").xnoremap
+
