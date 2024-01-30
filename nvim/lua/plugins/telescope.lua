605b396c6f40752bbbd887d1b428f597d9a93e20
Hung Hoang <hoanghun5@gmail.com>
2024-01-30 22:33:28 +0100

    NVIM telescope close buffer

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,21 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local actions = require("telescope.actions")
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-x>"] = actions.delete_buffer,
+          },
+        },
+        file_ignore_patterns = {
+          "node_modules",
+        },
+        hidden = true,
+      },
+    })
+  end
 }
