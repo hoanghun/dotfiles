5e61827c9514bb73d2cbe2e98ac221161ea83e04
Hung Hoang <hoanghun5@gmail.com>
2024-01-30 11:43:51 +0100

    NVIM oil

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup({
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-r>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = "actions.tcd",
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+        },
+        use_default_keymaps = false,
+      })
+    end,
+  },
+}
--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -4,3 +4,7 @@ local inoremap = require("user.keymap_utils").inoremap
 local tnoremap = require("user.keymap_utils").tnoremap
 local xnoremap = require("user.keymap_utils").xnoremap
 
+-- Map Oil to <leader>e
+nnoremap("<C-e>", function()
+	require("oil").toggle_float()
+end)
