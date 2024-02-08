8965bcd706ff6b8c1ea8624c2bbeec4be0e962be
Hung Hoang <hoanghun5@gmail.com>
2024-02-08 14:05:00 +0100

    Remove system clipboard

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -51,9 +51,6 @@ vim.opt.termguicolors = true
 -- Enable the sign column to prevent the screen from jumping
 vim.opt.signcolumn = "yes"
 
--- Enable access to System Clipboard
-vim.opt.clipboard = "unnamed,unnamedplus"
-
 -- Enable cursor line highlight
 vim.opt.cursorline = true
 
